087d397d979862dd76b58f485928555f250f9ef8
jibrankhairy <jibrankhairyy@gmail.com>
2025-05-30 14:03:37 +0700

    feat:Expose environment variables in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,24 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NODEMAILER_EMAIL: process.env.NODEMAILER_EMAIL,
+    NODEMAILER_PW: process.env.NODEMAILER_PW,
+    GITHUB_READ_USER_TOKEN_PERSONAL:
+      process.env.GITHUB_READ_USER_TOKEN_PERSONAL,
+    WAKATIME_API_KEY: process.env.WAKATIME_API_KEY,
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+    DOMAIN: process.env.DOMAIN,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 export default withNextIntl(nextConfig);
